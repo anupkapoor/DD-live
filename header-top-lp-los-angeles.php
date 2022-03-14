6ef4ff3b7a77802e916ee44c8d4f710d99e76502
rohanuve <101091236+rohanuve@users.noreply.github.com>
2022-03-14 17:27:47 +0530

    Los angles page Updates

---
+++ header-top-lp-los-angeles.php
@@ -0,0 +1,17 @@
+<?php include('config.php'); ?>	
+<div class="container-fluid header-top-lp">
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-white">
+			<a class="navbar-brand"  href="<?php echo BASE_URL ?>"><img src="<?php echo BASE_URL ?>/images/dd-logo.png" alt="Dedicated Developers Logo" class="responsive-img" /></a>
+			<div class="navbar-btn btn navbar-light bg-white lift ms-auto header-right-lp">
+				<div class="" id="navbarSupportedContent">
+				  <div class="d-flex">
+					<a class="top-links-lp current-link-lp" href="mailto:Newyork@DedicatedDevelopers.com"> <img src="../../images/email-icon-lp.png" /> losangeles@DedicatedDevelopers.com </a>
+					<a class="top-links-lp" href="tell:7702744482"> <img src="../../images/phone-icon-lp.png" class="hide-mobile" /> (770) 274-4482</a>
+					<a class="btn-default-lp" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Get a Quote</a>
+				  </div>
+				</div>
+			</div>
+		</nav>
+	</div>
+</div>
